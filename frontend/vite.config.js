2c9f5d0851baf0c80a0c491e0524aab536b9bf24
Karthik parnandi <kp7711589@gmail.com>
2025-02-04 15:38:15 +0530

    Configured for GitHub Pages

---
+++ frontend/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/TRENDY-NEEDS.github.io/', // Change 'your-repo-name' to your GitHub repo name
+});
---
+++ frontend/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    publicPath: '/TRENDY-NEEDS.github.io/' // Change 'your-repo-name' to your GitHub repo name
+  };
+  
